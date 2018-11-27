de364eb1e74f11c2716e5b037cfbd02c5c0371f8
Tre Logan <tr3.logan@gmail.com>
2018-11-27 01:25:43 -0500

    use std::pair instead of Point

--- main.cpp
+++ main.cpp
@@ -3,40 +3,14 @@
 
 #define KNIGHT_MOVES 8
 
-struct Point {
-    int x, y;
-
-    Point(int x, int y);
-};
-
-Point::Point(int x, int y) {
-    this->x = x;
-    this->y = y;
-}
-
-struct MoveSet {
-    Point *positions;
-
-    int getSize();
-};
-
-int MoveSet::getSize() {
-    return sizeof(positions);
-}
-
-struct ChessPiece {
-    int x, y;
-    MoveSet *moveSets;
-};
-
 int main() {
 
     int cases;
     int knightCount;
-    ChessPiece *knightArray;
-    ChessPiece king;
-    MoveSet knightMoveSet;
-    knightMoveSet.positions = new Point[KNIGHT_MOVES]{
+    std::pair<int, int> *knightArray;
+    std::pair<int,int > king;
+
+    std::pair<int, int> *moveSet = new std::pair<int, int>[KNIGHT_MOVES]{
             {-1, 2},
             {1,  2},
             {-1, -2},
@@ -51,27 +25,25 @@ int main() {
 
     std::cin >> knightCount;
 
-    knightArray = new ChessPiece[knightCount];
+    knightArray = new std::pair<int, int>[knightCount];
 
     for (int a = 0; a < knightCount; a++) {
-        ChessPiece knightPiece;
-        std::cin >> knightPiece.x >> knightPiece.y;
+        std::pair<int, int> knightPiece;
+        std::cin >> knightPiece.first >> knightPiece.second;
         knightArray[a] = knightPiece;
-
-        knightPiece.moveSets = &knightMoveSet;
     }
 
-    std::cin >> king.x >> king.y;
+    std::cin >> king.first >> king.second;
 
     for (int a = 0; a < knightCount; a++) {
         for (int b = 0; b < KNIGHT_MOVES; b++) {
-            printf("king x: %d, king y: %d\n", king.x, king.y);
+            printf("king x: %d, king y: %d\n", king.first, king.second);
 
-            int xMove = knightArray[a].x + knightMoveSet.positions[b].x;
-            int yMove = knightArray[a].y + knightMoveSet.positions[b].y;
+            int xMove = knightArray[a].first + moveSet[b].first;
+            int yMove = knightArray[a].second + moveSet[b].second;
             printf("knight x moves: %d, knight y moves: %d\n", xMove, yMove);
 
-            if (king.x == xMove && king.y == yMove) {
+            if (king.first == xMove && king.second == yMove) {
                 printf("YES");
                 exit(0);
             }
