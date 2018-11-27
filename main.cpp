4fca588552e62dde9127236e9aab536864755ca4
Tre Logan <tr3.logan@gmail.com>
2018-11-27 01:35:03 -0500

    minor optimization

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 
 #include <iostream>
 
-#define KNIGHT_MOVES 8
-
 int main() {
+
+    const int KNIGHT_MOVES = 8;
 
     int cases;
     int knightCount;
     std::pair<int, int> *knightArray;
-    std::pair<int,int > king;
+    std::pair<int, int> king;
 
+    /* All the move offsets of a knight from their current positions */
     std::pair<int, int> *moveSet = new std::pair<int, int>[KNIGHT_MOVES]{
             {-1, 2},
             {1,  2},
@@ -24,33 +25,30 @@ int main() {
     std::cin >> cases;
 
     std::cin >> knightCount;
-
     knightArray = new std::pair<int, int>[knightCount];
 
     for (int a = 0; a < knightCount; a++) {
-        std::pair<int, int> knightPiece;
-        std::cin >> knightPiece.first >> knightPiece.second;
-        knightArray[a] = knightPiece;
+        std::cin >> knightArray[a].first >> knightArray[a].second;
     }
 
     std::cin >> king.first >> king.second;
 
     for (int a = 0; a < knightCount; a++) {
         for (int b = 0; b < KNIGHT_MOVES; b++) {
+
             printf("king x: %d, king y: %d\n", king.first, king.second);
 
             int xMove = knightArray[a].first + moveSet[b].first;
             int yMove = knightArray[a].second + moveSet[b].second;
-            printf("knight x moves: %d, knight y moves: %d\n", xMove, yMove);
 
             if (king.first == xMove && king.second == yMove) {
-                printf("YES");
+                std::cout << "YES" << std::endl;
                 exit(0);
             }
         }
     }
 
-    printf("NO");
+    std::cout << "NO" << std::endl;
 
     return 0;
 }
